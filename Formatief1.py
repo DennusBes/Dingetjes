42f8a1e2bd650c93084bfc4d1904959ad79faea5
DennusBes <74312356+DennusBes@users.noreply.github.com>
2021-02-01 11:08:39 +0100

    Update Formatief1.py

--- Formatief1.py
+++ Formatief1.py
@@ -1,1 +1,9 @@
-# koffie tijd?
+#          OPDRACHT 1
+
+pyramide_size = int(input("Hoe groot? "))
+for i in range(1,pyramide_size):
+    print(f'{"*"*i}')
+for i in range(pyramide_size, 0,-1):
+    print(f'{"*" * i}')
+
+    
