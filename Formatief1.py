c50001d4f2f446f8561761036204f288f28d36db
DennusBes <74312356+DennusBes@users.noreply.github.com>
2021-02-01 11:14:58 +0100

    Update Formatief1.py

--- Formatief1.py
+++ Formatief1.py
@@ -1,9 +1,16 @@
 #          OPDRACHT 1
-
+# FOR LOOPS
 pyramide_size = int(input("Hoe groot? "))
 for i in range(1,pyramide_size):
     print(f'{"*"*i}')
-for i in range(pyramide_size, 0,-1):
+for i in range(pyramide_size, 0, -1):
     print(f'{"*" * i}')
+# WHILE LOOPS
+pyramide_counter = 1
+while pyramide_counter < pyramide_size:
+    print(f'{"*" * pyramide_counter}')
+    pyramide_counter += 1
+while pyramide_counter > 0:
+    print(f'{"*" * pyramide_counter}')
+    pyramide_counter -= 1
 
-    
