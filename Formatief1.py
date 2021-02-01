fc0516391529bd5836024f09580b1c1e11a6bac5
DennusBes <74312356+DennusBes@users.noreply.github.com>
2021-02-01 12:01:45 +0100

    Update Formatief1.py

--- Formatief1.py
+++ Formatief1.py
@@ -1,16 +1,35 @@
 #          OPDRACHT 1
-# FOR LOOPS
-pyramide_size = int(input("Hoe groot? "))
-for i in range(1,pyramide_size):
-    print(f'{"*"*i}')
-for i in range(pyramide_size, 0, -1):
-    print(f'{"*" * i}')
-# WHILE LOOPS
-pyramide_counter = 1
-while pyramide_counter < pyramide_size:
-    print(f'{"*" * pyramide_counter}')
-    pyramide_counter += 1
-while pyramide_counter > 0:
-    print(f'{"*" * pyramide_counter}')
-    pyramide_counter -= 1
 
+# FOR LOOPS
+def opdracht1forloop():
+    pyramide_size = int(input("Hoe groot? "))
+    for i in range(1,pyramide_size):
+        print(f'{"*"*i}')
+    for i in range(pyramide_size, 0, -1):
+        print(f'{"*" * i}')
+
+
+# WHILE LOOPS
+def opdracht1whileloop():
+    pyramide_size = int(input("Hoe groot? "))
+    pyramide_counter = 1
+    while pyramide_counter < pyramide_size:
+        print(f'{"*" * pyramide_counter}')
+        pyramide_counter += 1
+    while pyramide_counter > 0:
+        print(f'{"*" * pyramide_counter}')
+        pyramide_counter -= 1
+
+#          OPDRACHT 2
+def zoekdeverschillen():
+    text1 = (input("Voer text 1 in: "))
+    text2 = (input("Voer text 2 in: "))
+    if (len(text1)) > (len(text1)):
+        kortste_text = text2
+    else:
+        kortste_text = text1
+    for i in range(len(kortste_text)):
+        if text1[i]!=text2[i]:
+            print(f'Er vind een verschil plaats op index {i}.')
+
+zoekdeverschillen()
